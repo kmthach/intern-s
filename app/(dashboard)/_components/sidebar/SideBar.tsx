8c29c6394530366b9eb5a468577188c74abb04d0
Arphew Ng <kmthach.ai@gmail.com>
2024-10-17 07:23:49 +0000

    add action bar icons

--- app/(dashboard)/_components/sidebar/SideBar.tsx
+++ app/(dashboard)/_components/sidebar/SideBar.tsx
@@ -4,9 +4,9 @@ import { AccountManagementIcon, DashboardIcon } from "./Icons";
 import Navigation from "./Navigation";
 export default function SideBar() {
   return (
-    <div className="w-sidebar-expand flex flex-col items-center">
+    <div className="flex w-sidebar-expand flex-col items-center">
       {/* Title */}
-      <p className="text-title my-10 text-[26px] font-bold">Intern System</p>
+      <p className="my-10 text-[26px] font-bold text-title">Intern System</p>
       <Divider orientation="horizontal" className="bg-grey" />
 
       {/* Navigation */}
